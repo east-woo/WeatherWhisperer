0e8fc0fa6b4eeb14dd06769e807e9f9c80b759e1
dongwoo <dwseo96@naver.com>
2024-01-24 00:18:09 +0900

    :green_heart: gitignore 설정 후 재배포

---
+++ src/main/java/com/eastwoo/WeatherWhisperer/WeatherWhispererApplication.java
@@ -0,0 +1,15 @@
+package com.eastwoo.WeatherWhisperer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class WeatherWhispererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherWhispererApplication.class, args);
+	}
+
+}
