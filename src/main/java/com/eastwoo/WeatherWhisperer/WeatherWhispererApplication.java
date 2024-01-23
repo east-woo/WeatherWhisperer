2f0cdcd04ad8b94a383569e8726fade8c4c924ea
dongwoo <dwseo96@naver.com>
2024-01-23 17:40:24 +0900

    :sparkles: backend(Spring boot), froent(react) 연동 (CQRS)

--- src/main/java/com/eastwoo/WeatherWhisperer/WeatherWhispererApplication.java
+++ src/main/java/com/eastwoo/WeatherWhisperer/WeatherWhispererApplication.java
@@ -2,8 +2,10 @@ package com.eastwoo.WeatherWhisperer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class WeatherWhispererApplication {
 
 	public static void main(String[] args) {
