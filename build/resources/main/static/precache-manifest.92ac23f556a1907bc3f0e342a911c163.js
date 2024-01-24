344f8d9bb460ce6142978142bfad64920360bed4
dongwoo <dwseo96@naver.com>
2024-01-24 18:17:05 +0900

    :poop: .env 파일 추가 및 모달창 수정 중

---
+++ build/resources/main/static/precache-manifest.92ac23f556a1907bc3f0e342a911c163.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595419ed300e0862652732330a5ee07b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff5872d200d785e1bd64",
+    "url": "/static/css/2.0d6dd090.chunk.css"
+  },
+  {
+    "revision": "f136e0505015f7da270b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ff5872d200d785e1bd64",
+    "url": "/static/js/2.1a3037d4.chunk.js"
+  },
+  {
+    "revision": "7b3b6acfe8c8fc6ee60f",
+    "url": "/static/js/3.fb74ae2a.chunk.js"
+  },
+  {
+    "revision": "f136e0505015f7da270b",
+    "url": "/static/js/main.afeffb9a.chunk.js"
+  },
+  {
+    "revision": "64f2c769d0fb4bc407b1",
+    "url": "/static/js/runtime~main.897afe99.js"
+  }
+]);
