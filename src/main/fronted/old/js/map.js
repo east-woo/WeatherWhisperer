c80802321c211719257c5fcdb1eefcd326a3a142
dongwoo <dwseo96@naver.com>
2024-02-04 18:35:28 +0900

    :bug: 불필요한 css 코드 삭제

--- src/main/fronted/old/js/map.js
+++ src/main/fronted/old/js/map.js
@@ -2,9 +2,14 @@ const initMap = () => {
     return new ol.Map({
         target: 'map',
         layers: [
+            // new ol.layer.Tile({
+            //     source: new ol.source.OSM() // Base Map (OpenStreetMap)
+            // })
             new ol.layer.Tile({
-                source: new ol.source.OSM() // Base Map (OpenStreetMap)
-            })
+    source: new ol.source.XYZ({ //vworld api 사용
+        url : 'https://xdworld.vworld.kr/2d/Base/service/{z}/{x}/{y}.png'
+    })
+})
         ],
         view: new ol.View({
             center: ol.proj.fromLonLat([128.07666015625014, 36.4696612599073]),
