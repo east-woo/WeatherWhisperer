030c17f5ba0d1360275769d6bb6cee2d88af3760
dongwoo <dwseo96@naver.com>
2024-02-19 11:31:30 +0900

    :card_file_box: 테이블 이름 변경

--- back/src/main/resources/table.sql
+++ back/src/main/resources/table.sql
@@ -1,73 +1,115 @@
--- Table to store user information
-CREATE TABLE users (
-                       user_id SERIAL PRIMARY KEY,
-                       username VARCHAR(50) NOT NULL,
-                       chat_id INT NOT NULL,
-                       CONSTRAINT unique_username UNIQUE (username),
-                       CONSTRAINT unique_chat_id UNIQUE (chat_id)
+create table public.tb_weather_data
+(
+    weather_id       integer                  default nextval('weather_data_weather_id_seq'::regclass) not null
+        constraint weather_data_pkey
+            primary key,
+    location_id      integer,
+    temperature      numeric(5, 2)                                                                     not null,
+    condition        varchar(50)                                                                       not null,
+    observation_time timestamp with time zone default CURRENT_TIMESTAMP
 );
 
--- Table to store user locations
-CREATE TABLE user_locations (
-                                location_id SERIAL PRIMARY KEY,
-                                user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-                                location_name VARCHAR(100) NOT NULL,
-                                latitude DECIMAL(9,6) NOT NULL,
-                                longitude DECIMAL(9,6) NOT NULL
+alter table public.tb_weather_data
+    owner to root;
+
+create table public.tb_users
+(
+    user_id  integer default nextval('users_user_id_seq'::regclass) not null
+        constraint users_pkey
+            primary key,
+    username varchar(50)                                            not null
+        constraint unique_username
+            unique,
+    chat_id  integer                                                not null
+        constraint unique_chat_id
+            unique
 );
 
--- Table to store scheduled notifications
-CREATE TABLE notifications (
-                               notification_id SERIAL PRIMARY KEY,
-                               user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-                               location_id INT REFERENCES user_locations(location_id) ON DELETE CASCADE,
-                               notification_time TIME NOT NULL
+alter table public.tb_users
+    owner to root;
+
+create table public.tb_user_locations
+(
+    location_id   integer default nextval('user_locations_location_id_seq'::regclass) not null
+        constraint user_locations_pkey
+            primary key,
+    user_id       integer,
+    location_name varchar(100)                                                        not null,
+    latitude      numeric(9, 6)                                                       not null,
+    longitude     numeric(9, 6)                                                       not null
 );
 
--- Table to store weather information
-CREATE TABLE weather_data (
-                              weather_id SERIAL PRIMARY KEY,
-                              location_id INT REFERENCES user_locations(location_id) ON DELETE CASCADE,
-                              temperature DECIMAL(5,2) NOT NULL,
-                              condition VARCHAR(50) NOT NULL,
-                              observation_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+alter table public.tb_user_locations
+    owner to root;
+
+create table public.tb_telegram_bot_info
+(
+    bot_token varchar not null,
+    chat_id   bigint  not null
 );
 
+alter table public.tb_telegram_bot_info
+    owner to root;
 
-CREATE TABLE public.telegram_bot_info (
-                                          bot_token varchar NOT NULL,
-                                          chat_id int8 NOT NULL
+create table public.tb_weather_area
+(
+    admdst_cd varchar(50) not null,
+    stdg_ctpv varchar(50) not null,
+    stdg_sgg  varchar(50) default NULL::character varying,
+    stdg_emd  varchar(50) default NULL::character varying,
+    grid_x    varchar(50) not null,
+    grid_y    varchar(50) not null,
+    lot_hr    varchar(50) not null,
+    lot_mint  varchar(50) not null,
+    lot_scnd  varchar(50) not null,
+    lat_hr    varchar(50) not null,
+    lat_mint  varchar(50) not null,
+    lat_scnd  varchar(50) not null,
+    lot_mlls  varchar(50) not null,
+    lat_mlls  varchar(50) not null
 );
 
-CREATE TABLE tb_weather_area (
-                                 ADMDST_CD VARCHAR(50) NOT NULL,
-                                 STDG_CTPV VARCHAR(50) NOT NULL,
-                                 STDG_SGG VARCHAR(50) NULL DEFAULT NULL,
-                                 STDG_EMD VARCHAR(50) NULL DEFAULT NULL,
-                                 GRID_X VARCHAR(50) NOT NULL,
-                                 GRID_Y VARCHAR(50) NOT NULL,
-                                 LOT_HR VARCHAR(50) NOT NULL,
-                                 LOT_MINT VARCHAR(50) NOT NULL,
-                                 LOT_SCND VARCHAR(50) NOT NULL,
-                                 LAT_HR VARCHAR(50) NOT NULL,
-                                 LAT_MINT VARCHAR(50) NOT NULL,
-                                 LAT_SCND VARCHAR(50) NOT NULL,
-                                 LOT_MLLS VARCHAR(50) NOT NULL,
-                                 LAT_MLLS VARCHAR(50) NOT NULL
+comment on column public.tb_weather_area.admdst_cd is '행정 지역 코드';
+
+comment on column public.tb_weather_area.stdg_ctpv is '시도';
+
+comment on column public.tb_weather_area.stdg_sgg is '시, 군, 구';
+
+comment on column public.tb_weather_area.stdg_emd is '읍, 면, 동';
+
+comment on column public.tb_weather_area.grid_x is '그리드 X';
+
+comment on column public.tb_weather_area.grid_y is '그리드 Y';
+
+comment on column public.tb_weather_area.lot_hr is '경도 (시)';
+
+comment on column public.tb_weather_area.lot_mint is '경도 (분)';
+
+comment on column public.tb_weather_area.lot_scnd is '경도 (초)';
+
+comment on column public.tb_weather_area.lat_hr is '위도 (시)';
+
+comment on column public.tb_weather_area.lat_mint is '위도 (분)';
+
+comment on column public.tb_weather_area.lat_scnd is '위도 (초)';
+
+comment on column public.tb_weather_area.lot_mlls is '경도 (초/100)';
+
+comment on column public.tb_weather_area.lat_mlls is '위도 (초/100)';
+
+alter table public.tb_weather_area
+    owner to root;
+
+create table public.tb_notifications
+(
+    notification_id   integer default nextval('notifications_notification_id_seq'::regclass) not null
+        constraint notifications_pkey
+            primary key,
+    user_id           integer,
+    location_id       integer,
+    notification_time time                                                                   not null
 );
 
-COMMENT ON TABLE tb_weather_area IS '엑셀 파일 값의 데이터베이스를 포함하는 테이블';
-COMMENT ON COLUMN tb_weather_area.ADMDST_CD IS '행정 지역 코드';
-COMMENT ON COLUMN tb_weather_area.STDG_CTPV IS '시도';
-COMMENT ON COLUMN tb_weather_area.STDG_SGG IS '시, 군, 구';
-COMMENT ON COLUMN tb_weather_area.STDG_EMD IS '읍, 면, 동';
-COMMENT ON COLUMN tb_weather_area.GRID_X IS '그리드 X';
-COMMENT ON COLUMN tb_weather_area.GRID_Y IS '그리드 Y';
-COMMENT ON COLUMN tb_weather_area.LOT_HR IS '경도 (시)';
-COMMENT ON COLUMN tb_weather_area.LOT_MINT IS '경도 (분)';
-COMMENT ON COLUMN tb_weather_area.LOT_SCND IS '경도 (초)';
-COMMENT ON COLUMN tb_weather_area.LAT_HR IS '위도 (시)';
-COMMENT ON COLUMN tb_weather_area.LAT_MINT IS '위도 (분)';
-COMMENT ON COLUMN tb_weather_area.LAT_SCND IS '위도 (초)';
-COMMENT ON COLUMN tb_weather_area.LOT_MLLS IS '경도 (초/100)';
-COMMENT ON COLUMN tb_weather_area.LAT_MLLS IS '위도 (초/100)';
+alter table public.tb_notifications
+    owner to root;
+
