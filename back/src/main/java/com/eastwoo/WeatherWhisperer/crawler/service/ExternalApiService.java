b7317bfe3b70b352fcadff61510df2cf3558b0c5
dongwoo <dwseo96@naver.com>
2024-04-24 18:17:32 +0900

    :bug: 코드 구조 아키텍쳐 변경 중 (Scheduler -> 인터페이스 구조)

--- back/src/main/java/com/eastwoo/WeatherWhisperer/crawler/service/ExternalApiService.java
+++ back/src/main/java/com/eastwoo/WeatherWhisperer/crawler/service/ExternalApiService.java
@@ -10,53 +10,17 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import reactor.core.publisher.Mono;
 
-@Service
-public class ExternalApiService {
 
-    @Value("${api-key.datagokr-api-key}")
-    private String datagokrApiKey;
+public interface ExternalApiService {
+    /*초단기 실황 조회*/
+    public void getUltraSrtNcst();
 
-    /*초단기 예보 URL*/
-    @Value("${weather-api.endpoints.getUltraSrtFcst.url}")
-    private String ultraSrtFcstUrl;
+    /*초단기 예보 조회*/
+    public void getUltraSrtFcst();
 
-    /*단기 예보 URL*/
-    @Value("${weather-api.endpoints.getVilageFcst.url}")
-    private String vilageFcstUrl;
+    /*단기 예보 조회*/
+    public void getVilageFcst();
 
-
-
-    public void requestExternalApi() {
-        String base_date = "20240421";
-        String base_time = "0500";
-        String nx = "55";
-        String ny = "127";
-
-        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(vilageFcstUrl);
-        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
-
-        WebClient webClient = WebClient.builder().uriBuilderFactory(factory).baseUrl(vilageFcstUrl).build();
-
-        WeatherRequest request = WeatherRequest.builder(datagokrApiKey)
-                .baseDate(base_date)
-                .baseTime(base_time)
-                .nx(Integer.valueOf(nx))
-                .ny(Integer.valueOf(ny))
-                .build();
-
-        String response = webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .queryParam("serviceKey", request.getServiceKey())
-                        .queryParam("base_date", request.getBaseDate())
-                        .queryParam("base_time", request.getBaseTime())
-                        .queryParam("nx", request.getNx())
-                        .queryParam("ny", request.getNy())
-                        .queryParam("dataType", request.getDataType())
-                        .queryParam("pageNo", request.getPageNo())
-                        .queryParam("numOfRows", request.getNumOfRows())
-                        .build())
-                .retrieve().bodyToMono(String.class).block();
-
-        System.out.println(response);
-    }
+    /*예보 버전 조회*/
+    public void getFcstVersion();
 }
